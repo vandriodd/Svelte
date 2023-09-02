5f3459686c532b0988227c5dbd0168e5ae5fa0f1
vandriodd <ludasaadi@gmail.com>
2023-09-02 18:42:17 -0300

    Update '+page.svelte' component

--- app/src/routes/hello/+page.svelte
+++ app/src/routes/hello/+page.svelte
@@ -4,4 +4,7 @@
 	export let data: PageData;
 </script>
 
+{data.title}
+{data.text}
+
 <!-- On first load: rendered on server, updates: rendered on client, so... This component is used for building UI & runs on both client and server sides -->
