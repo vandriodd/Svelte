3990daf497e967ef336a73cb209ab9f40de9b8cd
vandriodd <ludasaadi@gmail.com>
2023-09-02 17:56:56 -0300

    Add comment about '+page.svelte' component in hello/ route

---
+++ app/src/routes/hello/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import type { PageData } from "./$types";
+
+	export let data: PageData;
+</script>
+
+<!-- On first load: rendered on server, updates: rendered on client, so... This component is used for building UI & runs on both client and server sides -->
