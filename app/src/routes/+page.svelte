38f7d3f4ab3e043f5b891502d7678b846a62dfbe
vandriodd <ludasaadi@gmail.com>
2023-09-02 17:27:23 -0300

    Add comment about '+page.svelte' components

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,2 +1,6 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+
+<!-- This component ('+page.svelte') defines a page of the app. By default its rendered both on the server (initial request) and in the browser (navigation) -->
