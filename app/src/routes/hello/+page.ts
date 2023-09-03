790f36a81547f7fef3c4ff1c151c50c9aeb1c251
vandriodd <ludasaadi@gmail.com>
2023-09-02 22:41:08 -0300

    Space

--- app/src/routes/hello/+page.ts
+++ app/src/routes/hello/+page.ts
@@ -6,7 +6,7 @@ export const load: PageLoad = async () => {
   // Since this function runs on the client side, it is not possible to do things such as:
   // - Access environment variables
   // - Use the Firebase admin SDK
-  // This component is ideal for fetching data, especially if it is public. It's the best defaultfor data fetching.
+  // This component is ideal for fetching data, especially if it is public. It's the best default for data fetching.
 
   const { title, text } = await fetch('someAPI').then(res => res.json());
 
