12a5c5654035795a6b004e9d36e6ce91ac8ee16b
vandriodd <ludasaadi@gmail.com>
2023-09-02 22:40:41 -0300

    Add comment about '+page.ts' component in hello/ route

---
+++ app/src/routes/hello/+page.ts
@@ -0,0 +1,17 @@
+// Exports a function that run both on the server (initial load) and the client (subsequent navigation) side
+import type { PageLoad } from './$types';
+
+export const load: PageLoad = async () => {
+
+  // Since this function runs on the client side, it is not possible to do things such as:
+  // - Access environment variables
+  // - Use the Firebase admin SDK
+  // This component is ideal for fetching data, especially if it is public. It's the best defaultfor data fetching.
+
+  const { title, text } = await fetch('someAPI').then(res => res.json());
+
+  return {
+    title,
+    text
+  };
+};
