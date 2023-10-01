aac8784ae052fc6cedb1f1943472e0a0d531d722
vandriodd <ludasaadi@gmail.com>
2023-09-30 22:51:23 -0300

    Verify Cookies

---
+++ build/src/routes/[username]/bio/+page.server.ts
@@ -0,0 +1,24 @@
+import type { PageServerLoad } from './$types';
+import { adminAuth, adminDB } from '$lib/server/admin';
+import { error } from '@sveltejs/kit';
+
+export const load = (async ({ cookies }) => {
+  const sessionCookie = cookies.get('__session');
+
+  try {
+    const decodedClaims = await adminAuth.verifySessionCookie(sessionCookie!);
+    const userDoc = await adminDB
+      .collection('users')
+      .doc(decodedClaims.uid)
+      .get();
+    const userData = userDoc.data();
+
+    return {
+      bio: userData?.bio,
+    };
+  } catch (e) {
+    console.log(e);
+    // redirect(301, '/login');
+    throw error(401, 'Unauthorized request!');
+  }
+}) satisfies PageServerLoad;
